26b68777a4b8eecb699ed3255babc9706a998821
daizu-007 <128668088+daizu-007@users.noreply.github.com>
2024-12-14 01:48:16 +0900

    pingコマンドの実装

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # audio_stream_discord_bot
-discordのVCをリアルタイムで取得したい
+discordのVCをリアルタイムでストリーミング
+
+# 目標
+- ボットが実行されているデバイスのマイク入力をVCに流す
+- VCの音声を実行デバイスのスピーカーから流す
+
+# クレジット
+以下の記事を参考にしました:  
+[【Discord.js V14】Discord BOTにスラッシュコマンドを実装してみる | Mori-chan](https://qiita.com/Mori-chan/items/2e869c0e2060b78f0380)
--- index.js
+++ index.js
@@ -1,20 +1,43 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
 require('dotenv').config();
 
 // クライアントを作成
-const client = new Client({ intents: [GatewayIntentBits.Guilds,  GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+// コマンドを格納するCollectionを作成
+client.commands = new Collection();
 
-// ボットが起動したときの処理
-client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
+// コマンドファイルを読み込む
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-// メッセージが送信されたときの処理
-client.on(Events.MessageCreate, message => {
-    if (message.content === 'ping') {
-        message.reply('pong!');
+// コマンドを登録
+for (const file of commandFiles) {
+    const command = require(path.join(commandsPath, file));
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    }
+}
+
+// コマンド実行処理
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;  // コマンド以外は無視
+    const command = interaction.client.commands.get(interaction.commandName);
+    // コマンドの実行とエラーハンドリング
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'エラーが発生しました', ephemeral: true });
     }
 });
 
+// ボットが起動したときの処理
+client.once(Events.ClientReady, readyClient => {
+	console.log(`${readyClient.user.tag}でログインしました。`);
+});
+
 // ボットをログイン
+console.log("起動しました。ログインします。");
 client.login(process.env.DISCORD_TOKEN);
